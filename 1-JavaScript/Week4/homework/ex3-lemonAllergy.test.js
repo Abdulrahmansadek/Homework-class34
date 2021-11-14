a0ddcb3a14cf55b129f5bc24e83d3a6335b73f61
Abdulrahmansadek <boody.sad@gmail.com>
2021-11-14 16:36:23 +0100

    resolving the issue in homework week3

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -35,22 +35,22 @@ function sanitizeFruitBasket(basket, fruits) {
 
 sanitizeFruitBasket(fruitBasket, 'lemon');
 
-// // ! Unit tests (using Jest)
+// ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(originalFruitBasketContents).toBe(fruitBasket);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    expect(result).toBe(expected);
   });
 });
