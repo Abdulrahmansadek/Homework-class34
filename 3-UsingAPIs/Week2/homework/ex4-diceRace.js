c5712da143b46ed40daf9129b6a73de2ddf522db
Abdulrahmansadek <boody.sad@gmail.com>
2021-11-29 14:33:18 +0100

    week2 homework api

--- 3-UsingAPIs/Week2/homework/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/homework/ex4-diceRace.js
@@ -15,17 +15,27 @@ const rollDice = require('../../helpers/pokerDiceRoller');
 
 function rollTheDices() {
   const dices = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDice()
+  const rolledDices = dices.map((dice) => rollDice(dice));
+  return Promise.race(rolledDices);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollTheDices()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollTheDices();
+    console.log('Resolved!', results);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
 main();
 
 // ! Do not change or remove the code below
 module.exports = rollTheDices;
+
+/**
+because our condition  if (roll < randomRollsToDo)
+ It does not matter which promise faster with the response here, our app keep our function running >
+ until the condition met!
+ */
